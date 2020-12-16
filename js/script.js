12d3911eb429a4d6bbef2526407d51d22d19cef4
Sasha <AlexanderEvgenDolgov@yandex.ru>
2020-12-16 20:31:06 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// let number = 4;
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// const persone ='Alex';
+
+// const bool= true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+
+// };
+
+// //console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr =['plum.png','orange.jpg','apple'];
+// console.log(arr[1]);
+    
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = +prompt("Вам есть 18?","18");
+// console.log(answer + 5);
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?',"");
+answers[1] = prompt('Как ваше фамилия?',"");
+answers[2] = prompt('Сколько вам лет?',"");
+
+document.write(answers);
+
+
+
+
