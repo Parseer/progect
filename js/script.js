c3856feb8f9b917229b158368f197cd78bf0c084
Sasha <AlexanderEvgenDolgov@yandex.ru>
2020-12-17 19:07:55 +0500

    two

--- js/script.js
+++ js/script.js
@@ -36,14 +36,88 @@
 // console.log(answer + 5);
 
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Как ваше имя?',"");
-answers[1] = prompt('Как ваше фамилия?',"");
-answers[2] = prompt('Сколько вам лет?',"");
+// answers[0] = prompt('Как ваше имя?',"");
+ // answers[1] = prompt('Как ваше фамилия?',"");
+// answers[2] = prompt('Сколько вам лет?',"");
 
-document.write(answers);
+// console.log(typeof(answers));
+
+// const category = "toys";
+// const add = "tree";
+
+// console.log(`https://someurl.com/${category}/${add}`);
 
 
+// console.log("arr" + "- object" );
+
+// console.log(4 + + "5");
+
+// let incr = 10,
+//     decr = 10;
+
+// //  ++incr;
+// //   --decr; 
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+
+// const isChecked = 1,
+//       isClose = 0;
+
+// console.log(isChecked && !isClose);
+
+// 1) Создать переменную numberOfFillms и в неё поместить ответ от пользователя 
+// на вопрос: Сколько фильмов вы уже посмотрели? 
+
+
+
+// const numberOfFillms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+// const personalMovieDB = {
+//         count : numberOfFillms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+// };
+
+// const a = prompt("Один из последний просмотренных фильмов?"),
+//       b = prompt("На сколько оцените его?"),
+//       c = prompt("Один из последний просмотренных фильмов?"),
+//       d = prompt("На сколько оцените его?");
+
+// personalMovieDB.log[a] = b;
+// personalMovieDB.log[c] = d;
+
+// console.log(personalMovieDB);
+
+
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
 
 
